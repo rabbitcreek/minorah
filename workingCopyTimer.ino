b621a9eacae943e157c4859d506407da89b28d57
rabbitcreek <rabbitcreek@gmail.com>
2024-03-17 18:12:58 -1000

    Add files via upload

--- workingCopyTimer.ino
+++ workingCopyTimer.ino
@@ -1,13 +1,11 @@
 #include <RTClib.h>
 // #include <Wire.h>
 RTC_DS3231 rtc;
-// the pin that is connected to SQW
-//#define CLOCK_INTERRUPT_PIN 4
 bool alarmCheck = 0;
 RTC_DATA_ATTR int primeIndex = 0;//primeIndex is the array index in holiday that holds the current year
 RTC_DATA_ATTR int primeDay = 0;//primeDay is the current day in the eight day holiday series 
- double holiday[11][4] = {{12,12,2023,1702443600},{3,17,2024,1710698400},{12,14,2025,1765774800},{12,4,2026,1796446800},{12,24,2027,1829710800},{12,12,2028,1860296400},{12,1,2029,1890882000},{12,20,2030,1924059600},{12,9,2031,1954645200},
-{11,27,2032,1985230800},{12,16,2033,2018408400}};//array of dates for next 11 years of hanukah
+ double holiday[14][4] = {{12,12,2023,1702443600},{3,18,2024,1710824400},{12,14,2025,1765774800},{12,4,2026,1796446800},{12,24,2027,1829710800},{12,12,2028,1860296400},{12,1,2029,1890882000},{12,20,2030,1924059600},{12,9,2031,1954645200},
+{11,27,2032,1985230800},{12,16,2033,2018408400},{12,6,2034,2049080400},{12,25,2035,2082258000},{12,13,2036,2112843600}};//array of dates for next 14 years of hanukah
 int ranFile[9] = {0,0,0,0,0,0,0,0,0}; //zeros out random array ranFile which is an array that randomises off time for candles
 #include <Adafruit_AW9523.h>
 Adafruit_AW9523 aw; //starts the aw9523 
@@ -29,11 +27,6 @@ void setup() {
     aw.pinMode(zip, AW9523_LED_MODE);
     aw.analogWrite(zip, 0);
   }
- 
- 
-
- 
-
     if(!rtc.begin()) {
         Serial.println("Couldn't find RTC!");
         //Serial.flush();
@@ -71,20 +64,13 @@ Serial.println(primeIndex);
     
 
 if(primeDay  == 0 || primeIndex == 0 ) firstDater();
-fakePrime = primeDay;   
-if(primeIndex != 0 && primeDay != 0 )startHoliday();       
-
-  
-
-
-
-
-
-    
-    
-   
-  
-  
+fakePrime = primeDay; 
+Serial.print("PimeDay = ");
+Serial.println(primeDay);
+Serial.print("PrimeIndex = ");
+Serial.println(primeIndex);
+     
+if(primeIndex != 0 && primeDay != 0 )startHoliday();         
 }
 
 void loop() {
@@ -119,7 +105,7 @@ void print_wakeup_reason()
 void firstDater(){ //this function finds the index in holiday for matching year if primeIndex is 0...like the first time
 DateTime cal = rtc.now();
 uint32_t unix_time = rtc.now().unixtime();
-for (int i = 0; i < 11; i++){ //goes through holiday index of years looking for this year...last year included so primeIndex will not equal 0..
+for (int i = 0; i < 15; i++){ //goes through holiday index of years looking for this year...last year included so primeIndex will not equal 0..
  if(  holiday[i][2] == cal.year()){ //checks to  see if its this year..
   primeIndex = i;//sets primeIndex to current year data
   break;
@@ -145,8 +131,8 @@ seeUNxtYr();
 }
 
 void startHoliday(){
-  int candleHold = 5000;    // delay in between lighting next candle
-  int candleTimer = 1 * 1000 * 60;   //ten minutes that candles will be on
+  int candleHold = 10000;    // delay in between lighting next candle
+  int candleTimer = 10 * 1000 * 60;   //ten minutes that candles will be on
   
   
   aw.analogWrite(9, 255);
@@ -184,7 +170,7 @@ if(primeDay == 9) {
   primeIndex ++;  //going for next year on list
   seeUNxtYr();
 } //if over 8 days schedule next year
-DateTime celebrate = rtc.now() + TimeSpan(0,0,1,0); //celebrate is dateTime for one day from now timespan function takes (day,hour,minute,sec)
+DateTime celebrate = rtc.now() + TimeSpan(1,0,0,0); //celebrate is dateTime for one day from now timespan function takes (day,hour,minute,sec)
 goToSleepDate(celebrate); //goes to sleep with wakup on correct date/hour/min/seconds match
 
 
@@ -226,9 +212,9 @@ void seeUNxtYr(){
 DateTime now = rtc.now();
 aw.analogWrite(9, 0);
 int p = primeIndex;
-if(p >= 10)while(1); //over ten years ... hopefully I will still be alive
+if(p >= 15)while(1); //over ten years ... hopefully I will still be alive
 //DateTime alarmTime (2021, 2, 17, 18, 59, 0);
-DateTime future (holiday[p][2],holiday[p][0], holiday[p][1],8, 0,0); //sets up alarm for the next set of numbers contained in array holiday...
+DateTime future (holiday[p][2],holiday[p][0], holiday[p][1],19, 0,0); //sets up alarm for the next set of numbers contained in array holiday...
 //if(now.year() +1 != holiday[p][2]) Serial.println("Next year schedule is broken"); //if now.year +1 does not correspond to next year something is wrong...
 goToSleepDate(future); //send future to sleep function...
 }
